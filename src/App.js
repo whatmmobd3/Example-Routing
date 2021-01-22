09cf9d67c85d3a34a4bcc7472cd5ffca24736979
Loc Nguyen <>
2021-01-22 13:49:22 +0700

    pass 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Route} from 'react-router-dom'
+import {Route, NavLink} from 'react-router-dom'
 
 import Courses from './containers/Courses/Courses';
 import Users from './containers/Users/Users';
@@ -18,7 +18,18 @@ class App extends Component {
           <li>Add a 404 error page and render it for any unknown routes</li>
           <li>Redirect requests to /all-courses to /courses (={'>'} Your "Courses" page)</li>
         </ol>
-        
+
+        <nav>
+          <ul style={{listStyle:'none', margin:'auto', padding:'0'}}>
+            <li style={{margin:'10px',display:'inline-block'}}>
+              <NavLink to="/courses">Courses</NavLink>
+            </li>
+            <li style={{margin:'10px',display:'inline-block'}}>
+              <NavLink to="/users">Users</NavLink>
+            </li>
+          </ul>
+        </nav>
+
         <Route path="/courses" component={Courses}/>
         <Route path="/Users" component={Users}/>
 
--- src/containers/Courses/Courses.js
+++ src/containers/Courses/Courses.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+
 import './Courses.css';
 
 class Courses extends Component {
